6f8172e33d4fa3403c67f284f579aae383afb5e8
BitBourne <mauriciocor@proton.me>
2025-12-07 16:59:38 -0600

    update patientLayout

--- Client/src/features/Dashboard/patient/layout/PatientLayout.jsx
+++ Client/src/features/Dashboard/patient/layout/PatientLayout.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import useAuth from "../../../auth/hooks/useAuth";
+import Header from "../../../../core/ui/Layout/Header";
+
 
 const PatientLayout = () => {
   const { user, loading } = useAuth();
@@ -13,9 +15,7 @@ const PatientLayout = () => {
   return (
     <div className="min-h-screen bg-main-gradient-10 flex flex-col">
       <header className="w-full bg-white shadow-sm px-6 py-4 flex justify-between items-center">
-        <h1 className="flex items-center gap-2 text-3xl font-bold bg-gradient-to-r from-[#B490CA] to-[#5EE7DF] text-transparent bg-clip-text">
-          VitaLink <span className="text-lg text-gray-400 font-normal">| Paciente</span>
-        </h1>
+        <Header/>
       </header>
 
       <main className="w-11/12 max-w-6xl mx-auto py-5">
